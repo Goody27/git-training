5c30a7f5c625b0cd60843272410b1d43e7b9ef19
shinshin15 <b0531020@eng.kitakyu-u.ac.jp>
2022-11-14 16:32:41 +0900

    test

--- app.rb
+++ app.rb
@@ -5,15 +5,6 @@ get '/' do
   'Hello world!'
 end
 
-<<<<<<< HEAD
-get '/shinshin15' do
-  'Hello shinshin15'
-=======
-get '/tako20' do
-  'Hello tako20!'
-end 
-
-get '/Goody27' do
-  'Hello and yahhoi Goody27!'
->>>>>>> cf96fb2dfc133e9bd402b0cfabc4317b132a53ff
+get '/challecara' do
+  'Hello challecara!'
 end
